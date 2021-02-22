66d2ce4d015206a9a5715e9f245be382b487016e
Reuven Lerner <reuven@lerner.co.il>
2021-02-22 16:41:55 +0200

    gitautosync update 128

---
+++ flycheck_mytypecheck.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def hello(name: str) -> str:   # type hint or type annotation
+    return f'Hello, {name}!'
+
+
+
+print(hello('world'))
+print(hello(5))
+s = hello('world')
+print(s + 3)
