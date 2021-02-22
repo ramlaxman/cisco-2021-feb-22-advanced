8262b32935c3ce8c0d16bafe68fb5f1bbacdacaa
Reuven Lerner <reuven@lerner.co.il>
2021-02-22 16:42:14 +0200

    gitautosync update 130

--- mytypecheck.py
+++ mytypecheck.py
@@ -4,6 +4,12 @@ def hello(name: str) -> str:   # type hint or type annotation
     return f'Hello, {name}!'
 
 
+print(hello('world'))
+print(hello(5))
+s = hello('world')
+print(s + 3)
+
+
 def firstlast(data):
     return data[:1] + data[-1:]
 
@@ -11,9 +17,3 @@ def firstlast(data):
 print(firstlast('abcde'))
 print(firstlast([10, 20, 30, 40, 50]))
 print(firstlast((100, 200, 300)))
-
-
-print(hello('world'))
-print(hello(5))
-s = hello('world')
-print(s + 3)
